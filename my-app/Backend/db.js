ef6b8cb8348adb1fbafa386796b3d2a63d1b31ca
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2024-04-13 13:47:31 +0500

    connection from mongodb database

--- my-app/Backend/db.js
+++ my-app/Backend/db.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://shoaibmohtashim973:WlhEBVCBa4Myhnh5@cluster0.hf7fikt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://shoaibmohtashim973:WlhEBVCBa4Myhnh5@cluster0.hf7fikt.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
 const mongodb = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         if (err) console.log("---", err)
         else {
             console.log('connected succesfully')
+            const fetch_data = await mongoose.connection.db.collection('food_items')
+            fetch_data.find({}).toArray(function (err, data) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(data)
+                }
+            })
+
         }
     })
 
