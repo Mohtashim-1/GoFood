9517b30a5974768774378ad4b817c001a7c7cb6a
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2024-04-27 00:40:38 +0500

    testing of login function

--- Backend/db.js
+++ Backend/db.js
@@ -1,21 +1,36 @@
+// const mongoose = require('mongoose');
+// const mongoURI = 'mongodb+srv://shoaibmohtashim973:WlhEBVCBa4Myhnh5@cluster0.hf7fikt.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
+// const mongodb = async () => {
+//     await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+//         if (err) console.log("---", err)
+//         else {
+//             console.log('connected succesfully')
+//             const fetch_data = await mongoose.connection.db.collection('food_items')
+//             fetch_data.find({}).toArray(function (err, data) {
+//                 if (err) {
+//                     console.log(err)
+//                 } else {
+//                     console.log(data)
+//                 }
+//             })
+
+//         }
+//     })
+
+// }
+// module.exports = mongodb;
+
+
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://shoaibmohtashim973:WlhEBVCBa4Myhnh5@cluster0.hf7fikt.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
-const mongodb = async () => {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        if (err) console.log("---", err)
-        else {
-            console.log('connected succesfully')
-            const fetch_data = await mongoose.connection.db.collection('food_items')
-            fetch_data.find({}).toArray(function (err, data) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(data)
-                }
-            })
+const mongoURI = 'mongodb+srv://shoaibmohtashim973:WlhEBVCBa4Myhnh5@cluster0.hf7fikt.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0';
 
-        }
-    })
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI, { useNewUrlParser: true });
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+    }
+};
 
-}
-module.exports = mongodb;
+module.exports = connectToMongoDB;
--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 4000
-const mongodb = require('./db')
-mongodb();
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// const express = require('express')
+// const app = express()
+// const port = 4000
+// const mongodb = require('./db')
+// mongodb();
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+// })
+// app.use(express.json())
+// app.use('/api', require('./Routes/CreateUser'));
+
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require('express');
+const app = express();
+const port = 4000;
+const mongodb = require('./db');
+
+// CORS middleware
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
-app.use(express.json())
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+// Parse JSON bodies
+app.use(express.json());
+
+// Route middleware
 app.use('/api', require('./Routes/CreateUser'));
 
-
+// Start the server
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
